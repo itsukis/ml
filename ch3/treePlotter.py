e827c900c74869715daccf5bf224103e2e9bf373
jiyoung song <itsuki@jiyoungs-MacBook-Pro.local>
2014-12-24 13:35:04 +0900

    working in createPlot api

--- ch3/treePlotter.py
+++ ch3/treePlotter.py
@@ -5,15 +5,15 @@ leafNode = dict(boxstyle="round4", fc="0.8")
 arrow_agrs = dict(arrowstyle="<-")
 
 def plotNode(nodeTxt, centerPt, parentPt, nodeType):
-	createPlot.ax.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
+	createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
 							xytext=centerPt, textcoords='axes fraction',
 							va='center', ha='center', bbox=nodeType,
 							arrowprops=arrow_agrs)
 
-def createPlot():
+def createPlot_t():
 	fig = plt.figure(1, facecolor='white')
 	fig.clf()
-	createPlot.ax = plt.subplot(111, frameon=False)
+	createPlot.ax1 = plt.subplot(111, frameon=False)
 	plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
 	plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
 	
@@ -44,3 +44,59 @@ def getNumLeafs(myTree):
 
 	return numLeafs
 
+def getTreeDepth(myTree):
+	maxDepth = 0
+	firstStr = myTree.keys()[0]
+	secondDict = myTree[firstStr]
+
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__ == 'dict':
+			thisDepth = 1 + getTreeDepth(secondDict[key])
+		else:
+			thisDepth = 1
+		if thisDepth > maxDepth: maxDepth = thisDepth
+
+	return maxDepth
+
+def plotMidText(cntrPt, parentPt, txtString):
+	xMid = (parentPt[0] - cntrPt[0])/2.0 + cntrPt[0]
+	yMid = (parentPt[1] - cntrPt[1])/2.0 + cntrPt[1]
+
+	print '%s : (%f, %f)' % (txtString, xMid, yMid)
+	createPlot.ax1.text(xMid, yMid, txtString)
+	return
+
+def plotTree(myTree, parentPt, nodeTxt):
+	numLeafs = getNumLeafs(myTree)
+	firstStr = myTree.keys()[0]
+
+	cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW, plotTree.yOff)
+	print 'cntrPt ', cntrPt, 'parentPt ', parentPt
+
+	plotMidText(cntrPt, parentPt, nodeTxt)
+	plotNode(firstStr, cntrPt, parentPt, decisionNode)
+
+	secondDict = myTree[firstStr]
+	plotTree.yOff = plotTree.yOff - (1.0/plotTree.totalD)
+
+	for key in secondDict.keys():
+
+
+
+def createPlot(inTree):
+	fig = plt.figure(1, facecolor='white')
+	fig.clf()
+	axprops = dict(xticks=[], yticks=[])
+	createPlot.ax1 = plt.subplot(111, frameon=False)  #, **axprops)
+
+	plotTree.totalW = float(getNumLeafs(inTree))
+	plotTree.totalD = float(getTreeDepth(inTree))
+
+	plotTree.xOff = -0.5/plotTree.totalW
+	plotTree.yOff = 1.0
+
+	print 'Leafs %d, Depth %d' % (plotTree.totalW, plotTree.totalD)
+	print plotTree.xOff, plotTree.yOff
+
+	plotTree(inTree, (0.5, 1.0), '123')
+	plt.show()
